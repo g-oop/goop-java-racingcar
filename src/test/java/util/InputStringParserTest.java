1c8a9e8c3b52d7a79d6d915104a12f5f5f8cdd03
임재진(메가존) <mzjjlim@lgupluspartners.co.kr>
2024-01-08 00:30:35 +0900

    1단계 > String 클래스에 대한 학습 테스트 > 요구사항 3

--- src/test/java/util/InputStringParserTest.java
+++ src/test/java/util/InputStringParserTest.java
@@ -2,12 +2,12 @@ package util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 
 class InputStringParserTest {
 
@@ -29,4 +29,18 @@ class InputStringParserTest {
     }
 
     record BasicSource(String input, String[] expected) { }
+
+
+    @DisplayName("1단계 > String 클래스 > 요구사항 3")
+    @ParameterizedTest
+    @CsvSource(value = {
+        "abc,3",
+        "wxyz,4"
+    })
+    void basic(String input, int index) {
+        ThrowingCallable callable = () -> input.charAt(index);
+        assertThatThrownBy(callable).isInstanceOf(IndexOutOfBoundsException.class);
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(callable);
+    }
+
 }
