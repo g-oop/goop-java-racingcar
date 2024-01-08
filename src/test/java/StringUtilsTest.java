eea75cddece64e5b6d740962f35b5dae2394cbdf
Ppojin <hhjin1814@gmail.com>
2024-01-08 21:02:53 +0900

    test: 생명주기 어노테이션 제거

--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -1,7 +1,6 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,7 +8,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StringUtilsTest {
 
     @ParameterizedTest(name = "( \"{0}\" ) -> {1}")
