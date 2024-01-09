1aa78230a7f36fae2b161aaf399fad598dff9909
mz-bskoo <mz.bskoo@gmail.com>
2024-01-10 02:00:53 +0900

    racingcar step1
    - string class test
    - set collection test

---
+++ src/test/java/com/megazone/goop/racingcar/NumberSetTest.java
@@ -0,0 +1,44 @@
+package com.megazone.goop.racingcar;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberSetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void getSizeTest() {
+        System.out.printf("current size is %d %n", numbers.size());
+    }
+
+    @DisplayName("check whether it contains multiple values at once.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void isContainsTest(int param) {
+        assertThat(numbers.contains(param)).isTrue();
+    }
+
+    @DisplayName("check whether it contains multiple values at once.")
+    @ParameterizedTest
+    @CsvSource(value = {"1?true", "2?true", "3?true", "4?false", "5?false"}, delimiter = '?')
+    void isContainsMatchTest(int param, boolean expected) {
+        assertThat(numbers.contains(param)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/com/megazone/goop/racingcar/StringUtilTest.java
@@ -0,0 +1,44 @@
+package com.megazone.goop.racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class StringUtilTest {
+
+    private static final String TOKEN = ",";
+
+    @Test
+    void parseStringTest() {
+        assertThat("1,2".split(TOKEN)).containsExactly("1", "2");
+        assertThat("1".split(TOKEN)).containsExactly("1").hasSize(1);
+    }
+
+    @Test
+    void getRangeStringTest() {
+        String source = "(1,2)";
+        String expected = "1,2";
+
+        int lbIndex = source.indexOf("(");
+        int rbIndex = source.indexOf(")");
+        int start = lbIndex == -1 ? 0 : lbIndex + 1;
+        int end = rbIndex == -1 ? source.length() : rbIndex;
+
+        assertThat(source.substring(start, end)).isEqualTo(expected);
+    }
+
+    @DisplayName("catch an exception that occurs when an out-of-range character is retrieved.")
+    @Test
+    void getCharAtTest() {
+        int index = 5;
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+                "abc".charAt(index);
+            })
+            .withMessage("String index out of range: %s", index)
+            .withMessageContaining("index out of range")
+            .withNoCause();
+    }
+}
