63ac80034678262745befe0cefee33aa02fb3d6c
Ppojin <hhjin1814@gmail.com>
2024-01-10 19:49:11 +0900

    feature: create method StringAddCalculator#getDelemeter

--- src/main/java/com/ppojin/util/StringAddCalculator.java
+++ src/main/java/com/ppojin/util/StringAddCalculator.java
@@ -1,6 +1,7 @@
 package com.ppojin.util;
 
-import com.ppojin.util.StringUtils;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
@@ -10,5 +11,14 @@ public class StringAddCalculator {
         }
 
         return 1;
+    }
+
+    private static String getDelimiter(String text) {
+        String delimiter = ",|:";
+        Matcher m = Pattern.compile("^//(.)\n(.*)$").matcher(text);
+        if(m.find()){
+            return delimiter + "|" + m.group(1);
+        }
+        return delimiter;
     }
 }
