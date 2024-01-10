1940908f753657a114f8802ad2efbfc70406edd6
Ppojin <hhjin1814@gmail.com>
2024-01-10 20:28:54 +0900

    feature: success splitAndSum

--- src/main/java/com/ppojin/util/StringAddCalculator.java
+++ src/main/java/com/ppojin/util/StringAddCalculator.java
@@ -1,20 +1,39 @@
 package com.ppojin.util;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    private static final String delimiter = ",|:";
 
-    public static int splitAndSum(String numericStr) {
-        if (StringUtils.isEmpty(numericStr)) {
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(String text) {
+        if (StringUtils.isEmpty(text)) {
             return 0;
         }
 
-        return 1;
+        String postDelimiter = getDelimiter(text);
+
+        text = delimiter.equals(postDelimiter) ? text : text.substring(4);
+
+        String[] strings = StringUtils.splitByDelimiter(text, postDelimiter);
+
+        return Arrays.stream(strings)
+                .map(s -> {
+                    int n = Integer.parseInt(s);
+                    if (n < 0) {
+                        throw new RuntimeException("요소에 음수는 입력할 수 없습니다.");
+                    }
+                    return n;
+                })
+                .mapToInt(i -> i)
+                .sum();
     }
 
     private static String getDelimiter(String text) {
-        String delimiter = ",|:";
         Matcher m = Pattern.compile("^//(.)\n(.*)$").matcher(text);
         if(m.find()){
             return delimiter + "|" + m.group(1);
