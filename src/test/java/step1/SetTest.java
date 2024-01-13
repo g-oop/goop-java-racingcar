e38ea7a72027fb81e33b9e1a92606b81c9dfc54a
Hyoju Kim <qjqmf217@naver.com>
2024-01-14 02:04:25 +0900

    refactor: step1 - 학습테스트 실습

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -1,14 +1,14 @@
+package step1;
+
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 public class SetTest {
 
@@ -24,18 +24,23 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("SetTest > 크기 체크")
     void getSize_ShouldEqual() {
         assertThat(numbers.size()).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
+    @DisplayName("SetTest > 숫자 포함 체크")
     void isContains_ShouldReturnTrue(int input) {
         assertThat(numbers.contains(input)).isTrue();
+        assertThat(numbers).contains(input);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("SetTest > 숫자 포함 여부 체크")
     void isContains_ShouldEqual(int input, boolean expected) {
         assertThat(numbers.contains(input)).isEqualTo(expected);
     }
