b3694d91940984f9becea4aa6f21ab26e260e89f
김주언 <maineon@mz.co.kr>
2024-01-14 17:10:06 +0900

    refactor after code review

--- src/test/java/racingcar/learningtest/StringClassTest.java
+++ src/test/java/racingcar/learningtest/StringClassTest.java
@@ -4,75 +4,59 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringClassTest {
 
     @Test
     @DisplayName("요구사항 1 > split 테스트(1)")
     void splitStringByComma() {
-        // given
         String input = "1,2";
 
-        // when
         String[] output = input.split(",");
 
-        // then
         String[] expected = {"1", "2"};
-        assertThat(expected).containsExactly(output);
+        assertThat(output).containsExactly(expected);
     }
 
     @Test
     @DisplayName("요구사항 1 > split 테스트(2)")
     void splitStringWithOneElement() {
-        // given
         String input = "1";
 
-        // when
         String[] output = input.split(",");
 
-        // then
         String[] expected = {"1"};
-        assertThat(expected).containsExactly(output);
+        assertThat(output).containsExactly(expected);
 
     }
 
     @Test
     @DisplayName("요구사항 2 > substring 테스트")
     void substringEnclosedText() {
-        // given
         String enclosedString = "(1,2)";
 
-        // when
         int startIdx = enclosedString.indexOf("(") + 1;
         int endIdx = enclosedString.indexOf(")");
         String unenclosedString = enclosedString.substring(startIdx, endIdx);
 
-        // then
         String expected = "1,2";
         assertEquals(expected, unenclosedString);
     }
 
     @Test
     @DisplayName("요구사항 3 > whenExceptionThrown_thenSuccess")
-    void throwsExceptionWhenIndexOutOfRange() {
-        // given
+    void throwsExceptionWhenIndexOutOfRange2() {
         String input = "abc";
+        int validIndex = 1;
+        int invalidIndex = input.length() + 1;
 
-        // when & then
-        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
-            .isThrownBy(() -> {
-                int inputLength = input.length();
-                int invalidIndex = inputLength + 1;
+        char output = input.charAt(validIndex);
 
-                try {
-                    input.charAt(invalidIndex);
-                } catch (StringIndexOutOfBoundsException e){
-                    String message = String.format("Index: %d, Size: %d",invalidIndex, inputLength );
-                    throw  new StringIndexOutOfBoundsException(message);
-                }
+        char expected = 'b';
 
-            }).withMessageMatching("Index: \\d+, Size: \\d+");
+        assertThat(output).isEqualTo(expected);
+
+        assertThrows(StringIndexOutOfBoundsException.class, () -> input.charAt(invalidIndex));
     }
 }
