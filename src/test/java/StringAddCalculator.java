9fb10353f84a025c28a1205e201f891ef6b9ad1a
박수진 <psjj@mz.co.kr>
2024-01-15 10:03:16 +0900

    피드백 반영

--- src/test/java/StringAddCalculator.java
+++ src/test/java/StringAddCalculator.java
@@ -1,51 +1,38 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 public class StringAddCalculator {
 
-    @DisplayName("문자열 나눈뒤 합을 구함")
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.*)\n(.*)");
+    private static final String NUMBERS_REGEX = "^[0-9]*$";
+    private static final String REGEX = "[,|:]";
+
     public static int splitAndSum(String inputString) {
         if (inputString == null || inputString.isEmpty()) {
             return 0;
         }
-        Matcher matcher = Pattern.compile("//(;)\n(.*)").matcher(inputString);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(inputString);
         if (matcher.find()) {
             String delimiter = matcher.group(1);
             String[] tokens = matcher.group(2).split(delimiter);
-            return StringSplitAndSum(tokens);
+            return splitAndSumStrings(tokens);
         }
-        return returnsNumber(inputString);
-    }
-    @DisplayName("returnsNumberOrSumNumber호출 메서드")
-    private static int returnsNumber(String inputString){
-
-        return returnsNumberOrSumNumber(inputString);
+        return splitAndSumStrings(inputString.split(REGEX));
     }
 
-    @DisplayName("하나의 값을 리턴하거나 SumNumber를 리턴하는지 구함")
-    private static int returnsNumberOrSumNumber(String inputString){
-        String[] splitNumbers = inputString.split(",|:");
-        return StringSplitAndSum(splitNumbers);
-    }
-
-    @DisplayName("문자열 나눈뒤 Sum 실행")
-    private static int StringSplitAndSum(String[] splitNumbers) {
+    private static int splitAndSumStrings(String[] splitNumbers) {
         int result = 0;
         for(String number: splitNumbers){
-            validateNegativeNumber(Integer.parseInt(number));
-            result += Integer.parseInt(number);
+            int returnNonNegative = validateAndReturnNonNegative(number);
+            result += returnNonNegative;
         }
         return result;
     }
 
-    @DisplayName("음수일떄 Runtime 에러 실행")
-    private static void validateNegativeNumber(int number) {
-        if(number < 0){
+    private static int validateAndReturnNonNegative(String number) {
+        if(!number.matches(NUMBERS_REGEX)){
             throw new RuntimeException("음수값이 들어갈수없습니다.");
         }
+        return Integer.parseInt(number);
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,16 +1,16 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
 
-    @Test
-    public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void splitAndSum_null_또는_빈문자(String inputString) {
+        int result = StringAddCalculator.splitAndSum(inputString);
         assertThat(result).isEqualTo(0);
     }
 
@@ -39,7 +39,7 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    public void splitAndSum_negative() throws Exception {
+    public void splitAndSum_negative() {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
             .isInstanceOf(RuntimeException.class);
     }
