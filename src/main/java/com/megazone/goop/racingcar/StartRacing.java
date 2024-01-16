302ae668e038b88e0ec8083e379393e5d2b24f10
mz-bskoo <mz.bskoo@gmail.com>
2024-01-17 01:33:08 +0900

    feat: 라운드수와 자동차수 입력 기능 구현

---
+++ src/main/java/com/megazone/goop/racingcar/StartRacing.java
@@ -0,0 +1,11 @@
+package com.megazone.goop.racingcar;
+
+import com.megazone.goop.racingcar.ui.InputView;
+
+public class StartRacing {
+
+    public static void main(String[] args) {
+        InputView iv = new InputView();
+        iv.setRules();
+    }
+}
