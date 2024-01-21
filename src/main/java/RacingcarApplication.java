22cf8e579a8b82676df9589504cf46736b7ca1ed
박수진 <psjj@mz.co.kr>
2024-01-21 19:10:51 +0900

    자동차 개수 요구사항 횟수 요구사항 , 랜덤값 요구사항 , 몇 대의 자동차 몇번 이동 구현 , 자동차 상태 화면 출력 구현

---
+++ src/main/java/RacingcarApplication.java
@@ -0,0 +1,11 @@
+import domain.ui.RacingGame;
+
+public class RacingcarApplication {
+
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.play();
+    }
+
+
+}
---
+++ src/main/java/domain/util/OutputHandler.java
@@ -0,0 +1,10 @@
+package domain.util;
+
+import java.util.Scanner;
+
+public class OutputHandler {
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
