1cbce9883284f3cd35414b29f5be6fb57d93109e
Taehyun Kim <k1m743hyun@icloud.com>
2024-01-21 16:15:34 +0900

    feature(RacingCar.java) Add the requirement of '전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다.'

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class RacingCar {
 
@@ -10,14 +11,81 @@ public class RacingCar {
         this.cars = new int[cars];
     }
 
-    public void run(int tries) {
+    public void stop() {
+
         for (int i = 0; i < this.cars.length; i++) {
-            this.cars[i] += tries;
+            stop(i);
         }
     }
 
-    public void stop() {
+    public void go() {
 
+        for (int i = 0; i < this.cars.length; i++) {
+            go(i);
+        }
+    }
+
+    public void start(int tries, int seed) {
+
+        for (int i = 0; i < tries; i++) {
+            run(seed);
+            ResultView.printResult(this.cars);
+        }
+    }
+
+    public void run(int seed) {
+
+        for (int i = 0; i < this.cars.length; i++) {
+            if (isStop(seed)) {
+                stop(i);
+            } else {
+                go(i);
+            }
+        }
+    }
+
+    private boolean isStop(int seed) {
+
+        Random random = new Random(seed);
+        return random.nextInt(10) < 4;
+    }
+
+    public int start(int tries) {
+
+        int stopCount = 0;
+        for (int i = 0; i < tries; i++) {
+            stopCount += run();
+            ResultView.printResult(this.cars);
+        }
+        return stopCount;
+    }
+
+    public int run() {
+
+        int stopCount = 0;
+        for (int i = 0; i < this.cars.length; i++) {
+            if (isStop()) {
+                stop(i);
+                stopCount += 1;
+            } else {
+                go(i);
+            }
+        }
+        return stopCount;
+    }
+
+    private boolean isStop() {
+
+        Random random = new Random();
+        return random.nextInt(10) < 4;
+    }
+
+    public void stop(int c) {
+        this.cars[c] += 0;
+    }
+
+    public void go(int c) {
+        this.cars[c] += 1;
     }
 
     public int getResult() {
