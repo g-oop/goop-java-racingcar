0e47d7189d8fe14b2af3aa0ffb8777c223484cb2
Taehyun Kim <k1m743hyun@icloud.com>
2024-01-21 16:13:36 +0900

    feature(InputView.java) Add the requirement of '사용자는 몇 대의 자동차로 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.'

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int inputCars() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return this.scanner.nextInt();
+    }
+
+    public int inputTries() {
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return this.scanner.nextInt();
+    }
+}
