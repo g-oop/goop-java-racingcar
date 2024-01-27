2e2241b650f7e1c46ce8e11016174a8f9372d7e4
Hyoju Kim <qjqmf217@naver.com>
2024-01-27 12:58:16 +0900

    refactor: 잘못된 값 입력 시 예외 처리

--- src/main/java/step1/racingcar/GameApp.java
+++ src/main/java/step1/racingcar/GameApp.java
@@ -6,8 +6,14 @@ import step1.racingcar.view.ResultView;
 public class GameApp {
 
     public static void main(String[] args) {
-        int carCount = InputView.getCarCount();
-        int tryCount = InputView.getTryCount();
+        int carCount = 0;
+        int tryCount = 0;
+        try {
+            carCount = InputView.getCarCount();
+            tryCount = InputView.getTryCount();
+        } catch (NumberFormatException e) {
+            System.out.println("잘못된 값이 입력되었습니다.");
+        }
         CarManager carManager = new CarManager(carCount, tryCount);
         carManager.readyToStart();
         ResultView.printResult(carManager);
