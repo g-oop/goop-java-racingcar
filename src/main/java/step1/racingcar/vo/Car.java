11dcb4b04b7c6339288429bf205566919c30af02
Hyoju Kim <qjqmf217@naver.com>
2024-01-27 20:54:09 +0900

    test: 4 이상이면 전진, 4 미만이면 정지 수정

--- src/main/java/step1/racingcar/vo/Car.java
+++ src/main/java/step1/racingcar/vo/Car.java
@@ -1,5 +1,7 @@
 package step1.racingcar.vo;
 
+import java.util.Objects;
+
 import step1.racingcar.util.NumberGenerator;
 
 public class Car {
@@ -26,5 +28,22 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return position == car.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
--- src/test/java/step1/racingcar/RacingCarTest.java
+++ src/test/java/step1/racingcar/RacingCarTest.java
@@ -8,29 +8,31 @@ import step1.racingcar.util.MovableNumberGenerator;
 import step1.racingcar.vo.Car;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RacingCarTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"5,3,10"}, delimiter = ',')
+    @CsvSource(value = {"5,3"}, delimiter = ',')
     @DisplayName("자동차 게임 - 1. 자동차 생성 확인")
-    public void carCreateTest(int carCount, int tryCount, int condition) throws Exception {
+    public void carCreateTest(int carCount, int tryCount) throws Exception {
         CarManager carManager = new CarManager(carCount, tryCount);
         carManager.readyToStart();
-        //assertThat(carManager.getCarCount()).isEqualTo(carCount);
-        //assertThat(carManager.getTryCount()).isEqualTo(tryCount);
         assertThat(carManager.moveCars().size()).isEqualTo(carCount);
     }
 
     @Test
     @DisplayName("자동차 게임 - 2. 4 이상이면 전진, 4 미만이면 정지")
     public void carMoveTest() throws Exception {
-        Car movableCar = new Car();
-        movableCar.move(new MovableNumberGenerator());
-        assertThat(movableCar.getPosition()).isEqualTo(2);
+        Car beforeMoveCar = new Car();
 
-        Car immovableCar = new Car();
-        immovableCar.move(new ImmovableNumberGenerator());
-        assertThat(immovableCar.getPosition()).isEqualTo(1);
+        Car movingCar = new Car();
+        movingCar.move(new MovableNumberGenerator());
+        assertNotEquals(beforeMoveCar, movingCar);
+        assertEquals(movingCar.getPosition(), 2);
+
+        Car nonMovingCar = new Car();
+        nonMovingCar.move(new ImmovableNumberGenerator());
+        assertEquals(beforeMoveCar, nonMovingCar);
     }
 }
