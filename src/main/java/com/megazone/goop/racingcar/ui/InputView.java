40ddf641f44670e148859a2a5fc63e3809ad8861
mz-bskoo <mz.bskoo@gmail.com>
2024-01-31 01:44:53 +0900

    refactor: 상수값 변수 사용

--- src/main/java/com/megazone/goop/racingcar/ui/InputView.java
+++ src/main/java/com/megazone/goop/racingcar/ui/InputView.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class InputView {
 
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+    private static final String CAR_NAME_DELIMITER = ",";
+
     private List<String> carNames;
     private int roundCount;
 
@@ -11,11 +14,11 @@ public class InputView {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)를 기준으로 구분)");
-        String[] inNames = scanner.nextLine().split(",");
+        String[] inNames = scanner.nextLine().split(CAR_NAME_DELIMITER);
 
         carNames = Arrays.stream(inNames)
             .map(String::trim)
-            .map(a -> a.substring(0, Math.min(a.length(), 5)))
+            .map(a -> a.substring(0, Math.min(a.length(), CAR_NAME_MAX_LENGTH)))
             .toList();
 
         System.out.println("시도할 회수는 몇 회 인가요?");
