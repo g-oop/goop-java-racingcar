692533f58be86ed5c52bfb2dc593d845a2c51984
mz-bskoo <mz.bskoo@gmail.com>
2024-01-31 01:04:31 +0900

    feat: 자동차이름 입력기능 추가

--- src/main/java/com/megazone/goop/racingcar/ui/InputView.java
+++ src/main/java/com/megazone/goop/racingcar/ui/InputView.java
@@ -1,24 +1,29 @@
 package com.megazone.goop.racingcar.ui;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class InputView {
 
-    private int carCount;
+    private List<String> carNames;
     private int roundCount;
 
     public void setRules() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        carCount = scanner.nextInt();
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)를 기준으로 구분)");
+        String[] inNames = scanner.nextLine().split(",");
+
+        carNames = Arrays.stream(inNames)
+            .map(String::trim)
+            .map(a -> a.substring(0, Math.min(a.length(), 5)))
+            .toList();
 
         System.out.println("시도할 회수는 몇 회 인가요?");
         roundCount = scanner.nextInt();
     }
 
-    public int getCarCount() {
-        return carCount;
+    public List<String> getCarNames() {
+        return carNames;
     }
 
     public int getRoundCount() {
