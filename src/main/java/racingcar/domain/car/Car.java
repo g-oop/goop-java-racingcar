b10e3e068c0427d73cf03e93bdd863cacf1fa9d2
김주언 <maineon@mz.co.kr>
2024-02-13 22:27:55 +0900

    refactor : 코드리뷰 반영 (final)

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -7,13 +7,13 @@ public class Car {
     private final String name;
     private int position;
 
+    public Car(String name) {
+        this(name, 0);
+    }
+
     private Car(String name, int position) {
         this.name = name;
         this.position = position;
-    }
-
-    public Car(String name) {
-        this(name, 0);
     }
 
 
