417c05a742e5912f24112de83e5537bdcfdcf7bb
김주언 <maineon@mz.co.kr>
2024-02-25 01:56:49 +0900

    refactor : 테스트 코드 수정

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -1,10 +1,10 @@
 package racingcar.domain.car;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.entity.car.Car;
+import racingcar.domain.strategy.NumberGenerator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,20 +19,19 @@ public class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {4,5,6,7,8,9,10})
     @DisplayName("기준값 이상일 경우 자동차 이동")
-    void moveWhenRandomValueIsOverThreshold(int randomNumber) {
-        car.move(randomNumber);
-
+    void moveWhenRandomValueIsOverThreshold(int movableNumber){
+        NumberGenerator movableNumberGenerator = () -> movableNumber;
+        car  = car.move(movableNumberGenerator.generate());
         assertEquals(1, car.getPosition());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0,1,2,3})
     @DisplayName("기준값 미만일 경우 자동차 이동 X")
-    void stayWhenRandomValueIsUnderThreshold(int randomNumber) {
-        car.move(randomNumber);
-
+    void stayWhenRandomValueIsUnderThreshold(int immovableNumber){
+        NumberGenerator immovableNumberGenerator = () -> immovableNumber;
+        car  = car.move(immovableNumberGenerator.generate());
         assertEquals(0, car.getPosition());
     }
-
 
 }
